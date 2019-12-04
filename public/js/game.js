afdfbc0b6151cfcc7383b50ec7507bd72a4a791a
Chris <cjmarsh725@gmail.com>
2019-12-03 18:41:10 -0800

    Basic window added to body

--- public/js/game.js
+++ public/js/game.js
@@ -10,4 +10,10 @@
 
   PIXI.utils.sayHello(type);
 
+  //Create a Pixi Application
+  let app = new PIXI.Application({width: 256, height: 256});
+
+  //Add the canvas that Pixi automatically created for you to the HTML document
+  document.body.appendChild(app.view);
+
 }());
