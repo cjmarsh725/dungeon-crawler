119cd22c4c95500694ab8969757c2ab22e82b3dc
Chris <cjmarsh725@gmail.com>
2019-12-09 20:44:27 -0800

    Added renderer resizing to full screen dimensions

--- src/game.js
+++ src/game.js
@@ -1,12 +1,8 @@
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-  type = "canvas"
-}
+let app = new PIXI.Application();
 
-PIXI.utils.sayHello(type)
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
-//Create a Pixi Application
-let app = new PIXI.Application({width: 256, height: 256});
-
-//Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
